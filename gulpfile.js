546ab2744c2b84acf5434f7f9c3925127b70ab94
Niclas Ek <niclas.l.ek@gmail.com>
2016-10-05 15:54:26 +0200

    Done up to and including step 24 in WhatHaveIDone.txt, i.e., install nodemon.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -32,4 +33,21 @@ gulp.task('inject', function() {
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+//Will run style and inject tasks before serve
+gulp.task('serve', ['style', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function(ev) {
+            console.log('Restarting...');
+        });
 });
