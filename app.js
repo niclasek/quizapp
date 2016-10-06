8f28ee13ecd6972e1271e92344ec66ae18992a9d
Niclas Ek <niclas.l.ek@gmail.com>
2016-10-06 17:48:49 +0200

    Installed EJS templating engine. Step 25 and 26 in WhatHaveIDone.txt

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require('express');
 
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT ||  5000;
 app.listen(port, function(err){
     console.log('running server on port ' + port);
 });
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
 
-//Not used since the static is taking the route before
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('Hello world');
+    res.render('index', {title: 'Hello from render', list: ['a','b']});
 });
