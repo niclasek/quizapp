91374003b9cd36059f93c273518900bd9abbfd5b
niclas <niclas.l.ek@gmail.com>
2017-08-31 09:33:26 +0200

    Added a list of verbs that i display in the verbs view.
    You can also click on the verbs to go to their unique page. However, the template start to look strange when we display sub pages, i.e., /verbs/1. This is probably because it is a single page app template.

--- src/routes/verbRoutes.js
+++ src/routes/verbRoutes.js
@@ -4,15 +4,39 @@ var express = require('express');
 var verbRouter = express.Router();
 
 var router = function (nav) {
+    var verbs = [
+        {
+            verb: 'andare',
+            type: 'essere'
+        },
+        {
+            verb: 'prendere',
+            type: 'avere'
+        },
+        {
+            verb: 'cominciare',
+            type: 'avere'
+        }
+    ];
     verbRouter.route('/')
         .get(function (req, res) {
             //Will look for file articoli in ./src/views
             res.render('verbs', {
                 title: 'Hello from render',
-                nav: nav
+                nav: nav,
+                verbs: verbs
             });
         });
-
+    //The /:id will give us everything beyound /verbs/ when we are dealing with this route
+    verbRouter.route('/:id')
+        .get(function (req, res) {
+            var id = req.params.id;
+            res.render('verb', {
+                title: 'Hello from render',
+                nav: nav,
+                verb: verbs[id]
+            });
+        });
     return verbRouter;
 };
 
