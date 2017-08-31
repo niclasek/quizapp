54ab0f23751a0b5bab6fe5534c087b1d559a5135
niclas <niclas.l.ek@gmail.com>
2017-08-31 12:30:54 +0200

    Reading the verbs from the MongoDB instead from the array in the js-file

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var verbs = [
+    {
+        name: 'andare',
+        type: 'essere'
+        },
+    {
+        name: 'prendere',
+        type: 'avere'
+        },
+    {
+        name: 'cominciare',
+        type: 'avere'
+        }
+    ];
 
 var router = function (nav) {
     adminRouter.route('/addVerb')
-    .get(function(req,res) {
-        res.send('inserting verbs');
-    });
+        .get(function (req, res) {
+            var url = 'mongodb://localhost:27017/quizapp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('verbs');
+                collection.insertMany(verbs, function (err, results) {
+                    res.send(results);
+                    db.close();
+                });
+            });
+            //res.send('inserting verbs');
+        });
     return adminRouter;
 };
 
--- src/routes/verbRoutes.js
+++ src/routes/verbRoutes.js
@@ -1,40 +1,44 @@
 var express = require('express');
-
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 //Used to create the routes for /articles
 var verbRouter = express.Router();
 
 var router = function (nav) {
-    var verbs = [
-        {
-            verb: 'andare',
-            type: 'essere'
-        },
-        {
-            verb: 'prendere',
-            type: 'avere'
-        },
-        {
-            verb: 'cominciare',
-            type: 'avere'
-        }
-    ];
     verbRouter.route('/')
         .get(function (req, res) {
-            //Will look for file articoli in ./src/views
-            res.render('verbs', {
-                title: 'Hello from render',
-                nav: nav,
-                verbs: verbs
+            var url = 'mongodb://localhost:27017/quizapp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('verbs');
+                collection.find({}).toArray(function (err, results) {
+                    //Will look for file verbs in ./src/views
+                    res.render('verbs', {
+                        title: 'Hello from render',
+                        nav: nav,
+                        verbs: results
+                    });
+                });
             });
         });
     //The /:id will give us everything beyound /verbs/ when we are dealing with this route
     verbRouter.route('/:id')
         .get(function (req, res) {
-            var id = req.params.id;
-            res.render('verb', {
-                title: 'Hello from render',
-                nav: nav,
-                verb: verbs[id]
+            var id = new objectId(req.params.id);
+
+            var url = 'mongodb://localhost:27017/quizapp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('verbs');
+
+                collection.findOne({
+                    _id: id
+                }, function (err, results) {
+                    //Will look for file verbs in ./src/views
+                    res.render('verb', {
+                        title: 'Hello from render',
+                        nav: nav,
+                        verb: results
+                    });
+                });
             });
         });
     return verbRouter;
